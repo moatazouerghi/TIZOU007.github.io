052445b11ab1521d2bb0195557f72d9292622568
moataz <mo3taz.werghi@gmail.com>
2023-08-18 11:45:07 +0200

    ajout url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Autres configurations Astro ici
+  // ...
+  buildOptions: {
+    // Définir le chemin de base pour GitHub Pages
+    basePath: '/mon-portfolio.github.io/'
+  }
+});
