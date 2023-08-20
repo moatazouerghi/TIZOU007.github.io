f5238f69bcbd6ca0bc925ef2371bb895f8315302
moataz <mo3taz.werghi@gmail.com>
2023-08-20 22:01:35 +0200

    Modif

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  // Autres configurations Astro ici
-  // ...
   buildOptions: {
-    // Définir le chemin de base pour GitHub Pages
-    basePath: '/mon-portfolio.github.io/'
+    basePath: '/TIZOU007.github.io/'
   }
 });
+
